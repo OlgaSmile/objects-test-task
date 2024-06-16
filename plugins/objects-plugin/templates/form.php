61eb8d62242920180fa7a9f53b78498b40b55e8d
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-06-16 22:05:40 +0300

    change language function to echo

--- plugins/objects-plugin/templates/form.php
+++ plugins/objects-plugin/templates/form.php
@@ -2,7 +2,7 @@
   <div class="filter_select_box">
 
     <div class="form-group">
-      <label class="filter_label" for="cat"><?php __('Район', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="cat"><?php _e('Район', 'objects-plugin'); ?></label>
       <select name="cat" id="cat" class="form-control">
         <?php echo '<option value="all">' . 'Всі райони' . '</option>';
             foreach ($district_options as $key => $label) {
@@ -12,63 +12,63 @@
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="title"><?php __('Назва', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="title"><?php _e('Назва', 'objects-plugin'); ?></label>
       <select name="title" id="title" class="form-control">
         <?php render_select_options($building_names, 'Всі назви'); ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="address"><?php __('Адреса', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="address"><?php _e('Адреса', 'objects-plugin'); ?></label>
       <select name="address" id="address" class="form-control">
         <?php render_select_options($building_addresses, 'Всі адреси'); ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="floor"><?php __('Кількість поверхів', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="floor"><?php _e('Кількість поверхів', 'objects-plugin'); ?></label>
       <select name="floor" id="floor" class="form-control">
         <?php render_select_options($floor_values, 'Всі'); ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="type"><?php __('Тип будинків', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="type"><?php _e('Тип будинків', 'objects-plugin'); ?></label>
       <select name="type" id="type" class="form-control">
         <?php render_select_options($type_values, 'Всі'); ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="ecology"><?php __('Екологічність', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="ecology"><?php _e('Екологічність', 'objects-plugin'); ?></label>
       <select name="ecology" id="ecology" class="form-control">
         <?php render_select_options($ecology_values, 'Всі варіанти'); ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="square"><?php __('Площа (м кв.)', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="square"><?php _e('Площа (м кв.)', 'objects-plugin'); ?></label>
       <select name="square" id="square" class="form-control">
         <?php render_select_options($square_values, 'Всі варіанти'); ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="rooms_amount"><?php __('Кількість кімнат', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="rooms_amount"><?php _e('Кількість кімнат', 'objects-plugin'); ?></label>
       <select name="rooms_amount" id="rooms_amount" class="form-control">
         <?php render_select_options($rooms_amount_values, 'Всі варіанти'); ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="balcony"><?php __('Є балкон', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="balcony"><?php _e('Є балкон', 'objects-plugin'); ?></label>
       <select name="balcony" id="balcony" class="form-control">
         <?php render_select_options($balcony_values, 'Всі'); ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label class="filter_label" for="bathroom"><?php __('Є санвузол', 'objects-plugin'); ?></label>
+      <label class="filter_label" for="bathroom"><?php _e('Є санвузол', 'objects-plugin'); ?></label>
       <select name="bathroom" id="bathroom" class="form-control">
         <?php render_select_options($bathroom_values, 'Всі'); ?>
       </select>
